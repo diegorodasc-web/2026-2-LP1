6e630d2f33ce2eaeefcf24cdee4f40caf3e9a125
diegorodasc-web <diego.rodas.c@uni.pe>
2026-09-09 18:46:33 -0500

    Add files via upload

---
+++ hola.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int edad=25;
+    float altura = 1.75;
+    char inicial = 'J';
+
+    printf("Hola mundo en C\n");
+    printf("Edad: %d años\n", edad);
+    printf("Altura: %.2f metros\n", altura);
+    printf("Inicial: %c\n", inicial);
+
+ return 0; 
+}
